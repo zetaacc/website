f02ab7d467974f1939c2ec035683f01088af1f70
Chris (Gentle) Y杨 <nodexy@gmail.com>
2023-12-26 16:26:35 +0800

    add gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-WDN8B2GSNN',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
