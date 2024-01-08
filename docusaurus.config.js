e80d84e63f99b85be3952647fa7975c6bcafca5c
Chris (Gentle) Y杨 <nodexy@gmail.com>
2024-01-08 17:44:27 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/zetaa-logo.png',
+  favicon: 'img/zetaa-logo-icon.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
